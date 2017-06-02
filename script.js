b5a53d16c665b0d86629cec5d7c5d118ba9bddea
Alex Solari <AlexSolari@users.noreply.github.com>
2017-06-02 15:57:55 +0300

    Update script.js
    Refactored code, now can also work with numbers

--- script.js
+++ script.js
@@ -1,49 +1,86 @@
-var inputArea = document.querySelector("#in");
-var resultsButton = document.querySelector("#results");
-var copiedMessage = document.querySelector("#message");
-var timeOuts = [];
-
-resultsButton.onclick = function(){
-    var result = inputArea.value.replace(/( )/g, "  ");
-    result = result.replace(/([a-z])/g, ":regional_indicator_$1: ");
-    
-    var textArea = document.createElement("textarea");
-    textArea.value = result;
-    document.body.appendChild(textArea);
-    textArea.select();
-    
-    try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-
-        showSuccessfulMessage();
-    } catch (err) {
-        alert('Oops, unable to copy');
-    }
-    document.getSelection().empty();
-    document.body.removeChild(textArea);
-}
-
-function showSuccessfulMessage(){
-    timeOuts.forEach(function(id){
-        clearTimeout(id);
-    });
-
-    copiedMessage.className = "";
-    for(var i = 0; i < 50; i++)
-    {
-        (function(value){
-            var timeoutId = setTimeout(function(){
-                copiedMessage.style.opacity = (50.0 - value) / 50.0;
-            }, 40*i);
-            timeOuts.push(timeoutId);
-        })(i)
+var Tool = (function(){
+    var BigLetterTool = {
+        inputArea: document.querySelector("#in"),
+        resultsButton: document.querySelector("#results"),
+        copiedMessage: document.querySelector("#message"),
         
-    }
+        timeOuts: [],
+        spacesRegex: /( )/g,
+        lettersRegex: /([a-z])/g,
+        numbersRegex: /([0-9])/g,
 
-    var timeoutId = setTimeout(function(){
-            copiedMessage.style.opacity = 1;
-            copiedMessage.className = "hidden";
-        }, 3000);
-    timeOuts.push(timeoutId);
-}
+        numbersDictionary:{
+            "0": ":zero: ",
+            "1": ":one: ",
+            "2": ":two: ",
+            "3": ":three: ",
+            "4": ":four: ",
+            "5": ":five: ",
+            "6": ":six: ",
+            "7": ":seven: ",
+            "8": ":eight: ",
+            "9": ":nine: ",
+        },
+
+        init: function(){
+            BigLetterTool.resultsButton.onclick = BigLetterTool.copyResult;
+        },
+        processText: function(value){
+            var result = value.replace(BigLetterTool.spacesRegex, "  ");
+            result = result.replace(BigLetterTool.lettersRegex, ":regional_indicator_$1: ");
+            result = result.replace(BigLetterTool.numbersRegex, function(match, substr){
+                return BigLetterTool.numbersDictionary[substr];
+            });
+
+            return result;
+        },
+        copyResult: function(){
+            var result = BigLetterTool.processText(BigLetterTool.inputArea.value);
+            
+            var textArea = document.createElement("textarea");
+            textArea.value = result;
+            document.body.appendChild(textArea);
+            textArea.select();
+            
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+
+                BigLetterTool.showSuccessfulMessage();
+            } catch (err) {
+                alert('Oops, unable to copy');
+            }
+
+            document.getSelection().empty();
+            document.body.removeChild(textArea);
+        },
+        showSuccessfulMessage: function(){
+            BigLetterTool.timeOuts.forEach(function(id){
+                clearTimeout(id);
+            });
+
+            BigLetterTool.copiedMessage.className = "";
+
+            for(var i = 0; i < 50; i++)
+            {
+                (function(value){
+                    var timeoutId = setTimeout(function(){
+                        BigLetterTool.copiedMessage.style.opacity = (50.0 - value) / 50.0;
+                    }, 40*i);
+                    
+                    BigLetterTool.timeOuts.push(timeoutId);
+                })(i);
+            }
+
+            var timeoutId = setTimeout(function(){
+                    BigLetterTool.copiedMessage.style.opacity = 1;
+                    BigLetterTool.copiedMessage.className = "hidden";
+                }, 3000);
+            BigLetterTool.timeOuts.push(timeoutId);
+        },
+    };
+
+    BigLetterTool.init();
+
+    return BigLetterTool;
+})();
